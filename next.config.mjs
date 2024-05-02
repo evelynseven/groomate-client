3a5327ea6cb819458065edda6dbafa917510d926
Evelyn Sun <evelynsun166@gmail.com>
2024-05-02 10:43:09 -0400

    modify baseUrl setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
-  async rewrites() {
-    return process.env.NODE_ENV === "production"
-      ? [
-          {
-            source: "/api/:path*",
-            destination: `http://10.0.0.244:3000/:path*`,
-          },
-        ]
-      : [
-          {
-            source: "/api/:path*",
-            destination: `http://localhost:3000/:path*`,
-          },
-        ];
-  },
+  // async rewrites() {
+  //   return process.env.NODE_ENV === "production"
+  //     ? [
+  //         {
+  //           source: "/api/:path*",
+  //           destination: `http://10.0.0.244:3000/:path*`,
+  //         },
+  //       ]
+  //     : [
+  //         {
+  //           source: "/api/:path*",
+  //           destination: `http://localhost:3000/:path*`,
+  //         },
+  //       ];
+  // },
 };
 
 export default nextConfig;
